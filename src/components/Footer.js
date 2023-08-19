bca42808c67754c722ef08b8d8e3f38a79885a7e
Odion Akhes <akhetuamhenodion@gmail.com>
2023-08-19 20:02:57 +0100

    sidebar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,18 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
 import styled from "styled-components";
 const Footer = () => {
   return (
     <Container>
-      <h5>&copy; {new Date().getFullYear()}</h5>
-
-            <span> StyleLoft</span>
+      <h5>
+        &copy; {new Date().getFullYear()}
+        <span> ComfySloth </span>
+      </h5>
+      <h5>All rights reserved</h5>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.footer`
   height: 5rem;
@@ -33,4 +37,5 @@ const Container = styled.footer`
     flex-direction: row;
   }
 `;
+
 export default Footer;
