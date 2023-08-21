e960d45ea31d0d0fe6503c9361b35fb6ad5eb31f
Odion Akhes <akhetuamhenodion@gmail.com>
2023-08-21 19:22:49 +0100

    set up filter and sort

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { CartProvider } from "./context/cart_context";
 import { ProductsProvider } from "./context/products_context";
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ProductsProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </ProductsProvider>
   </React.StrictMode>
 );
