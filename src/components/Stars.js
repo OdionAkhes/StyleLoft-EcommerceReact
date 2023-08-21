52be8334b2a647b18555a6b8c0a444ba1c5a1ffa
Odion Akhes <akhetuamhenodion@gmail.com>
2023-08-21 17:13:19 +0100

    starting cart

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,9 +1,42 @@
 import React from 'react'
-
-const Stars = () => {
+import styled from "styled-components";
+import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+const Stars = ({ stars, reviews }) => {
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5
+    return (
+      <span key={index}>
+        {stars > number ? (
+          <BsStarFill />) : stars > index ? (
+            <BsStarHalf/>
+          ) : (
+              <BsStar/>
+          
+        )}
+    </span>
+  )
+})
   return (
-    <div>Stars</div>
+    <Wrapper>
+      <div className="stars">
+       {tempStars}
+      </div>
+      <p className="reviews">({reviews} customer reviews)</p>
+    </Wrapper>
   )
 }
-
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  span {
+    color: #ffb900;
+    font-size: 1rem;
+    margin-right: 0.25rem;
+  }
+  p {
+    margin-left: 0.5rem;
+    margin-bottom: 0;
+  }
+  margin-bottom: 0.5rem;
+`;
 export default Stars
