ee1dfa9dcf1c798716f3d76916679a6c82ee291a
Odion Akhes <akhetuamhenodion@gmail.com>
2023-08-21 11:20:16 +0100

    featured products

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,13 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
 
 const Loading = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <div className="section secton-center">
+      <div className="loading"></div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { FeaturedProducts, Hero, Services, Contact } from "../components";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <main>
+      <Hero />
+      <FeaturedProducts />
+      <Services />
+      <Contact/>
+</main>
   )
 }
 
